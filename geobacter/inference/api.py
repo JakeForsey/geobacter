4ff2591d6b8f8d02f698d45cfdbfe29556d4ea52
jake <jakeforsey@googlemail.com>
2020-03-14 16:28:22 +0000

    Fix api.

--- geobacter/inference/api.py
+++ geobacter/inference/api.py
@@ -1,18 +1,22 @@
 import asyncio
 import os
+from pathlib import Path
 import threading
 
 from flask import Flask
 from flask_restful import reqparse, Api, Resource, abort
 import httpx
+import numpy as np
 import torch
 
+from geobacter.inference.geotypes import Point
 from geobacter.inference.util import buffer_point
-from geobacter.inference.mapnik import request_extent
+from geobacter.inference.mapnik import get_extent
 from geobacter.inference.networks.resnet import ResNetEmbedding
 from geobacter.inference.datasets.osm import BASE_TRANSFORMS
 
-CHECKPOINT = 'checkpoints/ResNetTriplet-OsmTileDataset-6a2d4729-e31b-4866-a67a-1c708a7aa485_embedding_136.pth'
+CACHE_DIR = Path("data/cache")
+CHECKPOINT = 'checkpoints/ResNetTriplet-OsmTileDataset-c448224c-a38e-4c02-8b8c-572ff00e21db_embedding_45297.pth'
 embedding_model = ResNetEmbedding(16, pretrained=False)
 embedding_model.load_state_dict(torch.load(CHECKPOINT))
 embedding_model.zero_grad()
@@ -29,9 +33,9 @@ def abort_if_token_incorrect(token: str):
 
 
 parser = reqparse.RequestParser()
-parser.add_argument('token', type=str)
-parser.add_argument('lon', type=float, help='lon must be a float.')
-parser.add_argument('lat', type=float, help='lat must be a float.')
+parser.add_argument('token', type=str, help='provide authentication token.', required=True)
+parser.add_argument('lon', type=float, help='lon must be a float.', required=True)
+parser.add_argument('lat', type=float, help='lat must be a float.', required=True)
 
 
 class Embeddings(Resource):
@@ -40,24 +44,25 @@ class Embeddings(Resource):
         abort_if_token_incorrect(args["token"])
         point = args["lon"], args["lat"]
 
-        async with httpx.AsyncClient() as client:
-            image_coroutine = request_extent(
-                buffer_point(point, 100.0),
-                16,
-                client
-            )
-            loop = asyncio.new_event_loop()
-            task = loop.create_task(image_coroutine)
-            try:
-                image = loop.run_until_complete(task)
-            except httpx.exceptions.NetworkError:
-                abort(404, message=f"Mapnik server is down.")
+        async def point_to_embedding(point: Point) -> np.ndarray:
+            async with httpx.AsyncClient() as client:
+                image = await get_extent(
+                    buffer_point(point, 100.0),
+                    CACHE_DIR,
+                    16,
+                    client
+                )
+                image = BASE_TRANSFORMS(image)
+                image = image.cuda()
+                return embedding_model(image.unsqueeze(0)).detach().cpu().numpy()
 
-        image = BASE_TRANSFORMS(image)
-        image = image.cuda()
+        loop = asyncio.new_event_loop()
+        task = loop.create_task(point_to_embedding(point))
+        embeddings = loop.run_until_complete(task)
+
         with model_semaphore:
             return {
-                "embeddings": embedding_model(image.unsqueeze(0)).detach().cpu().numpy().tolist(),
+                "embeddings": embeddings.tolist(),
                 "checkpoint": CHECKPOINT,
                 "lon": point[0],
                 "lat": point[1]
@@ -68,4 +73,4 @@ api.add_resource(Embeddings, '/embeddings')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, threaded=False)
